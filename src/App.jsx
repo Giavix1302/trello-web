c7b06b70597b73d657b8db187aeb267222280909
Giavix1302 <nguyenhoanggiavi123@gmail.com>
2025-02-14 18:59:34 +0700

    Dark light #1

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,29 @@ import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
 
+import {
+  useColorScheme
+} from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
 function App() {
   return (
     <>
+      <ModeToggle />
+
+      <hr />
       <p>Gia vi</p>
 
       <Typography variant="body2" color="text.secondary">  h1. Heading </Typography>
@@ -15,7 +35,7 @@ function App() {
       <Button variant="contained">Contained </Button>
       <Button variant="outlined">Outlined</Button>
       <AccessAlarm />
-      <br/>
+      <hr />
       <HomeIcon />
       <HomeIcon color="primary" />
       <HomeIcon color="secondary" />
