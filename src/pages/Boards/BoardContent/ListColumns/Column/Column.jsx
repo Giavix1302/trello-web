f04daa485eabbd1eece268e2c35d5182a3d3683c
Giavix1302 <nguyenhoanggiavi123@gmail.com>
2025-06-24 16:55:44 +0700

    fix: prevent draggable on input and btns dndkit

--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -185,6 +185,7 @@ function Column({ column }) {
                 size="small"
                 variant='outlined'
                 autoFocus
+                data-no-dnd='true'
                 value={newCardTitle}
                 onChange={(e) => setNewCardTitle(e.target.value)}
                 sx={{
@@ -206,6 +207,7 @@ function Column({ column }) {
               />
               <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
                 <Button
+                  data-no-dnd='true'
                   onClick={addNewCard}
                   variant='contained'
                   color="success"
