0e77f8b4193cbc76ce4a4e07b267f21618bb3f5c
Giavix1302 <nguyenhoanggiavi123@gmail.com>
2025-07-02 15:30:27 +0700

    fix build_mode dev and production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,10 @@
-export const API_ROOT = 'http://localhost:1302'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:1302'
+}
+
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-backend-cnqy.onrender.com'
+}
+// export const API_ROOT = 'http://localhost:1302'
+export const API_ROOT = apiRoot
